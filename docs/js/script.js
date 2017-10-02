96c2c477c62164483cc8a7678d022097b1b81cc1
Tia <msksfo@gmail.com>
2017-10-02 11:41:22 -0700

    move everythinthing into docs folder. compress images. lower padding for mobile view on project description

---
+++ docs/js/script.js
@@ -0,0 +1,31 @@
+var expandButton = document.querySelectorAll('.expand-button');
+var closeButton = document.querySelectorAll('.close-btn');
+
+var gallery = document.querySelectorAll('.gallery');
+
+for ( var i = 0; i < expandButton.length; i++){
+	expandButton[i].addEventListener('click', function(e){
+		this.parentNode.nextElementSibling.nextElementSibling.style.display = 'block';
+		this.parentNode.nextElementSibling.nextElementSibling.nextElementSibling.style.display = 'block';
+	});
+}
+
+
+/*
+
+for ( var i = 0; i < expandButton.length; i++){
+	expandButton[i].addEventListener('click', function(e){
+		this.parentNode.nextElementSibling.nextElementSibling.classList.toggle('active');
+		this.parentNode.nextElementSibling.nextElementSibling.nextElementSibling.classList.toggle('active');
+	});
+}
+
+*/
+
+for (var i = 0; i < closeButton.length; i++){
+	closeButton[i].addEventListener('click', function(e){
+		this.style.display = 'none';
+		this.previousElementSibling.style.display = 'none';
+		window.scrollTo(0, this.parentNode.offsetTop);
+	});
+}
